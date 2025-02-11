4ed1ee835ed149be29d8bbefbbad3b504ceef989
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-02-11 14:20:31 +0000

    Client FindById

--- src/main/java/com/mayara/client_crud/controllers/ClientController.java
+++ src/main/java/com/mayara/client_crud/controllers/ClientController.java
@@ -1,4 +1,25 @@
 package com.mayara.client_crud.controllers;
 
+import com.mayara.client_crud.DTOs.ClientDTO;
+import com.mayara.client_crud.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/clients")
 public class ClientController {
+
+    @Autowired
+    ClientService clientService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto = clientService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 }
--- src/main/java/com/mayara/client_crud/services/ClientService.java
+++ src/main/java/com/mayara/client_crud/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.mayara.client_crud.services;
 
+import com.mayara.client_crud.DTOs.ClientDTO;
+import com.mayara.client_crud.entities.Client;
 import com.mayara.client_crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,10 @@ public class ClientService {
     @Autowired
     ClientRepository clientRepository;
 
+    public ClientDTO findById(Long id){
+        Client client= clientRepository.findById(id).get();
+        return new ClientDTO(client);
+    }
 
 
 }
