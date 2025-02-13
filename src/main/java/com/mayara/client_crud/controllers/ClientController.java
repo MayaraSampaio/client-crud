6132187f8b4d84ad37d90a694e22be2aaa048d8a
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-02-13 10:39:09 +0000

    Insert uri in POST method

--- src/main/java/com/mayara/client_crud/controllers/ClientController.java
+++ src/main/java/com/mayara/client_crud/controllers/ClientController.java
@@ -8,6 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -30,7 +33,9 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO client){
         client = clientService.insert(client);
-        return ResponseEntity.ok(client);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(client.getId()).toUri();
+        return ResponseEntity.created(uri).body(client);
     }
 
     @PutMapping(value = "/{id}")
